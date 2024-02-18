d9a1961d0e33a6aca58eaf4d136a1497f8700df8
codeGirl-stark <141328922+codeGirl-stark@users.noreply.github.com>
2024-02-18 14:15:05 +0100

    changes

--- src/medecin/views.py
+++ src/medecin/views.py
@@ -57,176 +57,177 @@ def stat (request) :
     
     medecin = get_object_or_404(Medecin, is_active=True)
     patients = DossierMedical.objects.filter(medecin=medecin.id).count()
+    
     homme = DossierMedical.objects.filter(medecin=medecin.id, sexe="M").count()
-    stathomme = int((homme / patients)) * 100
+    stathomme = int((homme / patients) * 100)
     femme = DossierMedical.objects.filter(medecin=medecin.id, sexe="F").count()
-    statfemme = int((femme / patients)) * 100
+    statfemme = int((femme / patients) * 100)
     
     ado = DossierMedical.objects.filter(age__range=age_ado).count()
-    statAdo = int((ado / patients)) * 100
+    statAdo = int((ado / patients) * 100)
     perso = DossierMedical.objects.filter(age__range=age_perso).count()
-    statPerso = int((perso / patients)) * 100
+    statPerso = int((perso / patients) * 100)
     adulte = DossierMedical.objects.filter(age__range=age_adultes).count()
-    statAdulte = int((adulte / patients)) * 100
+    statAdulte = int((adulte / patients) * 100)
     
     #compte par age de découverte
     agedec1 = DossierMedical.objects.filter(ageDecouverte__range=age1).count()
-    statAgedec1 = int((agedec1 / patients)) * 100
+    statAgedec1 = int((agedec1 / patients) * 100)
     agedec2 = DossierMedical.objects.filter(ageDecouverte__range=age2).count()
-    statAgedec2 = int((agedec2 / patients)) * 100
+    statAgedec2 = int((agedec2 / patients) * 100)
     agedec3 = DossierMedical.objects.filter(ageDecouverte__range=age3).count()
-    statAgedec3 = int((agedec3 / patients)) * 100
+    statAgedec3 = int((agedec3 / patients) * 100)
     agedec4 = DossierMedical.objects.filter(ageDecouverte__range=age4).count()
-    statAgedec4 = int((agedec4 / patients)) * 100
+    statAgedec4 = int((agedec4 / patients) * 100)
     agedec5 = DossierMedical.objects.filter(ageDecouverte__range=age5).count()
-    statAgedec5 = int((agedec5 / patients)) * 100
+    statAgedec5 = int((agedec5 / patients) * 100)
     agedec6 = DossierMedical.objects.filter(ageDecouverte__range=age6).count()
-    statAgedec6 = int((agedec6 / patients)) * 100
+    statAgedec6 = int((agedec6 / patients) * 100)
     
     #compte par circonstances
     circonstances1 = DossierMedical.objects.filter(medecin=medecin.id, circonstance="Nodule").count()
-    statci1 = int((circonstances1 / patients)) * 100
+    statci1 = int((circonstances1 / patients) * 100)
     circonstances2 = DossierMedical.objects.filter(medecin=medecin.id, circonstance="Decouverte fortuite").count()
-    statci2 = int((circonstances2 / patients)) * 100
+    statci2 = int((circonstances2 / patients) * 100)
     circonstances3 = DossierMedical.objects.filter(medecin=medecin.id, circonstance="GMN").count()
-    statci3 = int((circonstances3 / patients)) * 100
+    statci3 = int((circonstances3 / patients) * 100)
     circonstances4 = DossierMedical.objects.filter(medecin=medecin.id, circonstance="ADP").count()
-    statci4 = int((circonstances4 / patients)) * 100
+    statci4 = int((circonstances4 / patients) * 100)
     circonstances5 = DossierMedical.objects.filter(medecin=medecin.id, circonstance="Metastase").count()
-    statci5 = int((circonstances5 / patients)) * 100
+    statci5 = int((circonstances5 / patients) * 100)
     circonstances6 = DossierMedical.objects.filter(medecin=medecin.id, circonstance="Autres").count()
-    statci6 = int((circonstances6 / patients)) * 100
+    statci6 = int((circonstances6 / patients) * 100)
     
     #compte par type hostoligique 
     type1 = DossierMedical.objects.filter(medecin=medecin.id, typeHisto="Papillaire").count()
-    statType1 = int((type1 / patients)) * 100
+    statType1 = int((type1 / patients) * 100)
     type2 = DossierMedical.objects.filter(medecin=medecin.id, typeHisto="Vesiculaire").count()
-    statType2 = int((type2 / patients)) * 100
+    statType2 = int((type2 / patients) * 100)
     type3 = DossierMedical.objects.filter(medecin=medecin.id, typeHisto="PapilloVesiculaire").count()
-    statType3 = int((type3 / patients)) * 100
+    statType3 = int((type3 / patients) * 100)
     type4 = DossierMedical.objects.filter(medecin=medecin.id, typeHisto="Medullaire").count()
-    statType4 = int((type4 / patients)) * 100
+    statType4 = int((type4 / patients) * 100)
     type5 = DossierMedical.objects.filter(medecin=medecin.id, typeHisto="Anaplasique").count()
-    statType5 = int((type5 / patients)) * 100
+    statType5 = int((type5 / patients) * 100)
     type6 = DossierMedical.objects.filter(medecin=medecin.id, typeHisto="Autres").count()
-    statType6 = int((type6 / patients)) * 100
+    statType6 = int((type6 / patients) * 100)
     
     #compte par classifications T
     clasTx = DossierMedical.objects.filter(medecin=medecin.id, clasT="Tx").count()
-    statTx = int((clasTx / patients)) * 100
+    statTx = int((clasTx / patients) * 100)
     clasT0 = DossierMedical.objects.filter(medecin=medecin.id, clasT="T0").count()
-    statT0 = int((clasT0 / patients)) * 100
+    statT0 = int((clasT0 / patients) * 100)
     clasT1a = DossierMedical.objects.filter(medecin=medecin.id, clasT="T1a").count()
-    statT1a = int((clasT1a / patients)) * 100
+    statT1a = int((clasT1a / patients) * 100)
     clasT1b = DossierMedical.objects.filter(medecin=medecin.id, clasT="T1b").count()
-    statT1b = int((clasT1b / patients)) * 100
+    statT1b = int((clasT1b / patients) * 100)
     clasT2 = DossierMedical.objects.filter(medecin=medecin.id, clasT="T2").count()
-    statT2 = int((clasT2 / patients)) * 100
+    statT2 = int((clasT2 / patients) * 100)
     clasT3a = DossierMedical.objects.filter(medecin=medecin.id, clasT="T3a").count()
-    statT3a = int((clasT3a / patients)) * 100
+    statT3a = int((clasT3a / patients) * 100)
     clasT3b = DossierMedical.objects.filter(medecin=medecin.id, clasT="T3b").count()
-    statT3b = int((clasT3b / patients)) * 100
+    statT3b = int((clasT3b / patients) * 100)
     clasT4a = DossierMedical.objects.filter(medecin=medecin.id, clasT="T4a").count()
-    statT4a = int((clasT3a / patients)) * 100
+    statT4a = int((clasT3a / patients) * 100)
     clasT4b = DossierMedical.objects.filter(medecin=medecin.id, clasT="T4b").count()
-    statT4b = int((clasT4b / patients)) * 100
+    statT4b = int((clasT4b / patients) * 100)
 
     #compte par classifications N
     clasNx = DossierMedical.objects.filter(medecin=medecin.id, clasN="Nx").count()
-    statNx = int((clasNx / patients)) * 100
+    statNx = int((clasNx / patients) * 100)
     clasN0 = DossierMedical.objects.filter(medecin=medecin.id, clasN="N0").count()
-    statN0 = int((clasN0 / patients)) * 100
+    statN0 = int((clasN0 / patients) * 100)
     clasN1a = DossierMedical.objects.filter(medecin=medecin.id, clasN="N1a").count()
-    statN1a = int((clasN1a / patients)) * 100
+    statN1a = int((clasN1a / patients) * 100)
     clasN1b = DossierMedical.objects.filter(medecin=medecin.id, clasN="N1b").count()
-    statN1b = int((clasN1b / patients)) * 100
+    statN1b = int((clasN1b / patients) * 100)
     
     #compte par classifications M
     clasMx = DossierMedical.objects.filter(medecin=medecin.id, clasM="Mx").count()
-    statMx = int((clasMx / patients)) * 100
+    statMx = int((clasMx / patients) * 100)
     clasM0 = DossierMedical.objects.filter(medecin=medecin.id, clasM="M0").count()
-    statM0 = int((clasM0 / patients)) * 100
+    statM0 = int((clasM0 / patients) * 100)
     clasM1 = DossierMedical.objects.filter(medecin=medecin.id, clasM="M1").count()
-    statM1 = int((clasM1 / patients)) * 100
+    statM1 = int((clasM1 / patients) * 100)
 
     #compte par métastase
     meta1 = DossierMedical.objects.filter(medecin=medecin.id, metastase="Aucune").count()
-    statmeta1 = int((meta1 / patients)) * 100
+    statmeta1 = int((meta1 / patients) * 100)
     meta2 = DossierMedical.objects.filter(medecin=medecin.id, metastase="Ganglionaire").count()
-    statmeta2 = int((meta2 / patients)) * 100
+    statmeta2 = int((meta2 / patients) * 100)
     meta3 = DossierMedical.objects.filter(medecin=medecin.id, metastase="Pulmonaire").count()
-    statmeta3 = int((meta3 / patients)) * 100
+    statmeta3 = int((meta3 / patients) * 100)
     meta4 = DossierMedical.objects.filter(medecin=medecin.id, metastase="Oseuse").count()
-    statmeta4 = int((meta4 / patients)) * 100
+    statmeta4 = int((meta4 / patients) * 100)
     meta5 = DossierMedical.objects.filter(medecin=medecin.id, metastase="Hepatique").count()
-    statmeta5 = int((meta5 / patients)) * 100
+    statmeta5 = int((meta5 / patients) * 100)
     meta6 = DossierMedical.objects.filter(medecin=medecin.id, metastase="Cerebrale").count()
-    statmeta6 = int((meta6 / patients)) * 100
+    statmeta6 = int((meta6 / patients) * 100)
     meta7 = DossierMedical.objects.filter(medecin=medecin.id, metastase="Autres").count()
-    statmeta7 = int((meta7 / patients)) * 100
+    statmeta7 = int((meta7 / patients) * 100)
     
     #compte par stade
     stade1 = DossierMedical.objects.filter(medecin=medecin.id, stade="I").count()
-    statStade1 = int((stade1 / patients)) * 100
+    statStade1 = int((stade1 / patients) * 100)
     stade2 = DossierMedical.objects.filter(medecin=medecin.id, stade="II").count()
-    statStade2 = int((stade2 / patients)) * 100
+    statStade2 = int((stade2 / patients) * 100)
     stade3 = DossierMedical.objects.filter(medecin=medecin.id, stade="III").count()
-    statStade3 = int((stade3 / patients)) * 100
+    statStade3 = int((stade3 / patients) * 100)
     stade4 = DossierMedical.objects.filter(medecin=medecin.id, stade="IVA").count()
-    statStade4 = int((stade4 / patients)) * 100
+    statStade4 = int((stade4 / patients) * 100)
     stade5 = DossierMedical.objects.filter(medecin=medecin.id, stade="IVB").count()
-    statStade5 = int((stade5 / patients)) * 100
+    statStade5 = int((stade5 / patients) * 100)
     
     #compte par risque
     risque1 = DossierMedical.objects.filter(medecin=medecin.id, risque="Faible").count()
-    statRisque1 = int((risque1 / patients)) * 100
+    statRisque1 = int((risque1 / patients) * 100)
     risque2 = DossierMedical.objects.filter(medecin=medecin.id, risque="Intermediare").count()
-    statRisque2 = int((risque2 / patients)) * 100
+    statRisque2 = int((risque2 / patients) * 100)
     risque3 = DossierMedical.objects.filter(medecin=medecin.id, risque="Haut").count()
-    statRisque3 = int((risque3 / patients)) * 100
+    statRisque3 = int((risque3 / patients) * 100)
 
     #compte par totalisation chirugicale
     totalChir1 = DossierMedical.objects.filter(medecin=medecin.id, totalChir="1 temps").count()
-    statChir1 = int((totalChir1 / patients)) * 100
+    statChir1 = int((totalChir1 / patients) * 100)
     totalChir2 = DossierMedical.objects.filter(medecin=medecin.id, totalChir="2 temps").count()
-    statChir2 = int((totalChir2 / patients)) * 100
+    statChir2 = int((totalChir2 / patients) * 100)
     totalChir3 = DossierMedical.objects.filter(medecin=medecin.id, totalChir="curage").count()
-    statChir3 = int((totalChir3 / patients)) * 100
+    statChir3 = int((totalChir3 / patients) * 100)
 
 
     #compte par nombre de curage
     nbrCur1 = DossierMedical.objects.filter(medecin=medecin.id, nbrCure__range=cure1).count()
-    statCur1 = int((nbrCur1 / patients)) * 100
+    statCur1 = int((nbrCur1 / patients) * 100)
     nbrCur2 = DossierMedical.objects.filter(medecin=medecin.id, nbrCure__range=cure2).count()
-    statCur2 = int((nbrCur2 / patients)) * 100
+    statCur2 = int((nbrCur2 / patients) * 100)
     nbrCur3 = DossierMedical.objects.filter(medecin=medecin.id, nbrCure__range=cure3).count()
-    statCur3 = int((nbrCur3 / patients)) * 100
+    statCur3 = int((nbrCur3 / patients) * 100)
     
     
     #compte par activités cumulés
     activite1 = DossierMedical.objects.filter(medecin=medecin.id, activiteCumule__range=cumul1).count()
-    statActivite1 = int((activite1 / patients)) * 100
+    statActivite1 = int((activite1 / patients) * 100)
     activite2 = DossierMedical.objects.filter(medecin=medecin.id, activiteCumule__range=cumul2).count()
-    statActivite2 = int((activite2 / patients)) * 100
+    statActivite2 = int((activite2 / patients) * 100)
     activite3 = DossierMedical.objects.filter(medecin=medecin.id, activiteCumule__range=cumul3).count()
-    statActivite3 = int((activite3 / patients)) * 100
+    statActivite3 = int((activite3 / patients) * 100)
     activite4 = DossierMedical.objects.filter(medecin=medecin.id, activiteCumule__range=cumul4).count()
-    statActivite4 = int((activite4 / patients)) * 100
+    statActivite4 = int((activite4 / patients) * 100)
     activite5 = DossierMedical.objects.filter(medecin=medecin.id, activiteCumule__range=cumul5).count()
-    statActivite5 = int((activite5 / patients)) * 100
+    statActivite5 = int((activite5 / patients) * 100)
     
     
     carcinome = DossierMedical.objects.filter(medecin=medecin.id, carcinome="oui").count()
-    statCarcinome = int((carcinome / patients)) * 100
+    statCarcinome = int((carcinome / patients) * 100)
     
     effra = DossierMedical.objects.filter(medecin=medecin.id, effraCapsulaire="oui").count()
-    statEffra = int((effra / patients)) * 100
+    statEffra = int((effra / patients) * 100)
     
     embon = DossierMedical.objects.filter(medecin=medecin.id, embonVasculaire="oui").count()
-    statEmbon = int((embon / patients)) * 100
+    statEmbon = int((embon / patients) * 100)
     
     refrac = DossierMedical.objects.filter(medecin=medecin.id, refrac="oui").count()
-    statRefrac = int((refrac / patients)) * 100
+    statRefrac = int((refrac / patients) * 100)
 
 
     if not patients :
